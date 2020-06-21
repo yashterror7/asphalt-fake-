7746f3e819f52118db105bb648bce0e76cfa49df
yashterror7 <63530120+yashterror7@users.noreply.github.com>
2020-06-21 20:38:28 +0530

    Add files via upload

---
+++ asphalt8/carRacingStage2.5-master/js/Player.js
@@ -0,0 +1,47 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+  getRank(){
+    var rankref = database.ref('endcars');
+    rankref .on("value",(data)=>{
+     this.rank = data.val();
+    })
+  }    
+  static updaterank(rank){
+    database.ref('/').update({
+      endcars: rank 
+    });
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
